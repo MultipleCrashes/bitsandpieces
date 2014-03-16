9d155177cda13bc889ec1879090cfa439dd3fd6f
Harish <inimitableharish@gmail.com>
2014-03-16 18:30:00 +0530

    First Push/

---
+++ test.py
@@ -0,0 +1,13 @@
+import time
+
+class Time:
+    def currentTime():
+        print " Current Time is "
+        print time.now()
+
+
+
+if __name__='__main__':
+    TimeObj=Time()
+    print " Calling time function "
+    TimeObj.currentTime()
